3cc90a671d2a7d959b9136d98df72dc00c0ac9a9
Donovan Jong Wen <djw9413@hotmail.com>
2020-09-13 14:36:52 +0800

    Final Changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,125 +1,135 @@
-// Assignment Code
+// Button Event Listener
 var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
 function writePassword() {
-  promptChoices();
-  var passwordText = document.querySelector("#password");
-  passwordText.textContent = password;
-
-  // Array List
-  let caseArray = [];
-  let upChr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z"];
-  let lowChr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  let specChr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "|", "[", "]", ";", "'", ":", "<", ">", "?", "/"];
-
   // Variable List
   var passResult = "";
-  var genUpChr;
-  var genLowChr;
-  var genNum;
-  var genSpecChr;
+  var upChr;
+  var lowChr;
+  var num;
+  var specChr;
   var passLength;
 
   // Function for password length validation
-  function promptChoices() {
-    let passLength = parseInt(prompt("Please select the amount of character your password require \n Limit on Password characters are 8 and 128."));
+  var passLength = parseInt(prompt("Please select the amount of character your password require \n Limit on Password characters are 8 and 128."));
 
-    if (passLength < 8 || passLength > 128) {
-      alert("Error: You have selected under or over the 8-128 character limit.");
-      passLength = prompt("Please retry selecting the amount of characters your password require \n Limit on Password characters are 8 and 128.");
-      return false;
-    }
+  if (passLength < 8 || passLength > 128) {
+    alert("Error: You have selected under or over the 8-128 character limit.");
+    passLength = prompt("Please retry selecting the amount of characters your password require \n Limit on Password characters are 8 and 128.");
+    return false;
+  }
 
-    if (isNaN(passLength)) {
-      alert("Error: Please select a correct amount.");
-      passLength = prompt("Please retry selecting the amount of characters your password require \n Limit on Password characters are 8 and 128.");
-      return false;
-    }
+  if (isNaN(passLength)) {
+    alert("Error: Please select a correct amount.");
+    passLength = prompt("Please retry selecting the amount of characters your password require \n Limit on Password characters are 8 and 128.");
+    return false;
+  }
 
-    else (alert("Password length is" + " " + passLength + " " + "characters."));
+  else (alert("Password length is" + " " + passLength + " " + "characters."));
 
-    // Confirmation of variables with prompts
+  // Confirmation of variables with prompts
 
-    genLowChr = confirm("Should your password include lowercase alphabet characters?");
-    if (genLowChr == true) {
-      var genLowChr = true;
-    }
-    else if (genLowChr == false) {
-      var genLowChr = false;
-    }
+  lowChr = confirm("Should your password include lowercase alphabet characters?");
+  if (lowChr == true) {
+    var lowChr = true;
+  }
+  else if (lowChr == false) {
+    var lowChr = false;
+  }
 
-    genUpChr = confirm("Should your password include uppercase alphabet characters?");
-    if (genUpChr == true) {
-      var genUpChr = true;
-    }
-    else if (genUpChr == false) {
-      var genUpChr = false;
-    }
+  upChr = confirm("Should your password include uppercase alphabet characters?");
+  if (upChr == true) {
+    var upChr = true;
+  }
+  else if (upChr == false) {
+    var upChr = false;
+  }
 
-    genNum = confirm("Should your password include numerical characters?");
-    if (genNum == true) {
-      var genNum = true;
-    }
-    else if (genNum == false) {
-      var genNum = false;
-    }
+  num = confirm("Should your password include numerical characters?");
+  if (num == true) {
+    var num = true;
+  }
+  else if (num == false) {
+    var num = false;
+  }
 
-    genSpecChr = confirm("Should your password include special characters?");
-    if (genSpecChr == true) {
-      var genSpecChr = true;
-    }
-    else if (genSpecChr == false) {
-      var genSpecChr = false;
-    }
+  specChr = confirm("Should your password include special characters?");
+  if (specChr == true) {
+    var specChr = true;
+  }
+  else if (specChr == false) {
+    var specChr = false;
+  }
 
-    while (genLowChr === false && genUpChr === false && genNum === false && genSpecChr === false) {
-      alert("Error: Character criteria not selected. Please restart!")
-      return false;
+  while (lowChr === false && upChr === false && num === false && specChr === false) {
+    alert("Error: Character criteria not selected. Please restart!")
+    return false;
+  }
+
+
+  //Password Generator
+  var passResult = "";
+  const lowArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const upArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z"];
+  const numArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const specArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "|", "[", "]", ";", "'", ":", "<", ">", "?", "/"];
+
+  function randomLow() {
+    if (lowChr === true) {
+      return lowArray[Math.floor(Math.random() * lowArray.length)];
     }
   }
 
-  // Array Grouping
-  if (genLowChr == true) {
-    caseArray.push(lowChr);
-
-  }
-  if (genUpChr == true) {
-    caseArray.push(upChr);
-
-  }
-  if (genNum == true) {
-    caseArray.push(num);
-
-  }
-  if (genSpecChr == true) {
-    caseArray.push(specChr);
-
+  function randomUp() {
+    if (upChr === true) {
+      return upArray[Math.floor(Math.random() * upArray.length)];
+    }
   }
 
-  //Password length loop
-  for (let i = 0; i < passLength; i++) {
-    let randomArrayNum;
-    let chosenCharArray;
-    let randomCharNum;
-    let randomChar;
-
-    randomArrayNum = parseInt(Math.floor(Math.random() * caseArray.length));
-
-    chosenCharArray = caseArray[randomArrayNum];
-
-    randomCharNum = Math.floor(Math.random() * chosenCharArray.length);
-
-    randomChar = selectedCharArray[randomCharNum];
-
-    passResult += randomChar;
-
+  function randomNum() {
+    if (num === true) {
+      return numArray[Math.floor(Math.random() * numArray.length)];
+    }
   }
 
-  document.getElementById("password").innerHTML = passResult;
+  function randomSpec() {
+    if (specChr === true) {
+      return specArray[Math.floor(Math.random() * specArray.length)];
+    }
+  }
 
+  for (var i = 0; i < passLength; i++) {
+
+    if (lowChr === true) {
+      var randomL = randomLow()
+    }
+    else {
+      randomL = ""
+    }
+    if (upChr === true) {
+      var randomU = randomUp()
+    }
+    else {
+      randomU = ""
+    }
+    if (num === true) {
+      var randomN = randomNum()
+    }
+    else {
+      randomN = ""
+    }
+    if (specChr === true) {
+      var randomS = randomSpec()
+    }
+    else {
+      randomS = ""
+    }
+
+    var passResult = passResult + randomL + randomU + randomN + randomS;
+  }
+  var passFinalResult = passResult.substring(0, passLength);
+
+  var passwordText = document.querySelector("#password");
+  passwordText.textContent = passFinalResult;
 }
